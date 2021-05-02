0f6c2ba507aa9e95086e3be226f72ac2546ed300
osyrisrblx <osyrisrblx@gmail.com>
2021-05-01 17:53:39 -0700

    Update fetch_latest.sh

--- fetch_latest.sh
+++ fetch_latest.sh
@@ -3,5 +3,5 @@
 VERSION=v0.0.16-alpha
 
 wget https://github.com/Sleitnick/Knit/releases/download/$VERSION/knit.zip
-unzip -o knit.zip
+unzip -o knit.zip -x "src/init.lua" "src/Util/Promise.lua"
 rm -rf knit.zip
