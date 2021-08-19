2555e7439edf4fd7790d88ea235b9f8bbab80f86
osyrisrblx <osyrisrblx@gmail.com>
2021-08-19 15:54:54 -0700

    Upgrade to knot 0.0.21-alpha

--- Knit/Util/Timer.d.ts
+++ Knit/Util/Timer.d.ts
@@ -1,8 +1,13 @@
 import Janitor from "./Janitor";
-import Signal from "./Signal";
+import Signal, { Connection } from "./Signal";
 
 interface Timer {
 	Tick: Signal;
+	Interval: number;
+	UpdateSignal: Signal;
+	TimeFunction: () => number;
+	AllowDrift: boolean;
+
 	Start(): void;
 	StartNow(): void;
 	Stop(): void;
@@ -12,6 +17,7 @@ interface Timer {
 interface TimerConstructor {
 	new (interval: number, janitor?: Janitor): Timer;
 	Is(obj: unknown): obj is Timer;
+	Simple: (interval: number, callback: () => void, updateSignal?: Signal, timeFunc?: () => number) => Connection
 }
 
 declare const Timer: TimerConstructor;
--- fetch_latest.sh
+++ fetch_latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=v0.0.20-alpha
+VERSION=v0.0.21-alpha
 
 wget https://github.com/Sleitnick/Knit/releases/download/$VERSION/knit.zip
 unzip -o knit.zip -x "Knit/init.lua" "Knit/Util/Promise.lua"
